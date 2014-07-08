68db53c7de7f75ca5278bd97941c90600c57be0e
kaka <huxinghai1988@gmail.com>
2014-07-08 22:11:17 +0800

    fixed avatar

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,20 +1,39 @@
-$(function(){
-	var canvas=document.getElementById("myavatar");
-	var g=canvas.getContext("2d");      
-	var img=new Image()
-	img.src="/images/avatar.jpeg"
-  	img.onload = function(){
-		g.drawImage(img, 0, 0, 420, 420, 0, 0 ,160, 160);        
-		var i = 0.1
-		g.lineWidth = 2           
-		g.globalAlpha=0.5;     
-		var clear_id = setInterval(function(){            
-			g.beginPath();       
-			g.strokeStyle = '#'+Math.floor(Math.random()*16777215).toString(16);            
-			g.arc(80, 80, 80, Math.PI * (i - 0.1), Math.PI * i)
-			g.stroke();
-		  	if(i >= 2) return clearInterval(clear_id);
-		  	i += 0.1
-		}, 50)
-	}
+
+window.detectmob = function() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
+$(function(){ 
+  var canvas=document.getElementById("myavatar");
+  var g=canvas.getContext("2d");      
+  var img=new Image()
+  img.src="/images/avatar.gif"
+  if(!detectmob()){
+    img.onload = function(){
+      g.drawImage(img, 0, 0, 160, 160, 0, 0 ,160, 160);        
+      var i = 0.1
+      g.lineWidth = 2           
+      g.globalAlpha=0.5;     
+      var clear_id = setInterval(function(){            
+        g.beginPath();       
+        g.strokeStyle = '#'+Math.floor(Math.random()*16777215).toString(16);            
+        g.arc(80, 80, 80, Math.PI * (i - 0.1), Math.PI * i)
+        g.stroke();
+          if(i >= 2) return clearInterval(clear_id);
+          i += 0.1
+      }, 50)
+    }
+  }
 })
